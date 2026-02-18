c5cd839c1e5152c90c3ad1ac8e8afb9a02265476
FONRRNOR <nocturne.5761@gmail.com>
2026-02-18 14:45:37 +0700

    feat: resolve conflict and add hero banner

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <div class="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors">
-  </div>
-</template>
+  <div
+    class="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors"
+  ></div>
+  <script setup>
+    import Navbar from "./components/Navbar.vue";
+    import HeroBanner from "./components/HeroBanner.vue";
+    import FeatureBar from "./components/FeatureBar.vue";
+  </script>
 
-<script setup>
-</script>
+  <template>
+    <Navbar />
+    <HeroBanner />
+    <FeatureBar />
+  </template>
+
+  <script setup></script>
+</template>
